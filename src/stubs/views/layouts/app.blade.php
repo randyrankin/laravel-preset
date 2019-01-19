635f4437b86f73e6b201377ef5bda7945ed35b33
Randy Rankin <rrrankin@gmail.com>
2019-01-19 08:22:35 -0600

    Nitpicks

--- src/stubs/views/layouts/app.blade.php
+++ src/stubs/views/layouts/app.blade.php
@@ -7,8 +7,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Custom Laravel object: used for accessing global PHP inside JS. -->
         <script>
             window.Laravel = {
                 appName: '{{ config('app.name') }}'
@@ -16,13 +14,9 @@
         </script>
     </head>
     <body class="antialiased bg-grey-lightest font-sans leading-tight text-black">
-        <!-- Top Bar -->
-        <div class="bg-purple pb-1"></div>
-
         <div id="app" v-cloak>
             @yield('body')
         </div>
-
         @stack('beforeScripts')
         <script src="{{ mix('js/manifest.js') }}"></script>
         <script src="{{ mix('js/vendor.js') }}"></script>
--- src/stubs/views/welcome.blade.php
+++ src/stubs/views/welcome.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('body')
-<div class="flex items-center justify-center min-h-screen ">
-    <div class="text-4xl text-purple">Laravel</div>
-    <div class="text-grey-dark">co-starring Vue, Tailwind and PurgeCSS</div>
+<div class="justify-center min-h-screen ">
+    <div class="flex items-center text-4xl text-purple">Laravel</div>
+    <div class="flex items-center text-grey-dark">co-starring Vue, Tailwind and PurgeCSS</div>
 </div>
 @endsection
